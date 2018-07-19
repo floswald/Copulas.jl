c3c83c05cdc1659f1a313fc85a41355d7a021040
JulienPascal <julien.pascal@sciencespo.fr>
2018-07-19 23:12:24 +0200

    fixing issue

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,14 @@ tolTest = 0.0001
 print("Testing with tolerance level = $(tolTest)")
 
 @testset "testing default constructor" begin
-	
+
 	ndim = 3
 	rho  = 0.5
 
 	c = NormalCopula(ndim,rho)
 	@test c.rho == rho
 	@test c.d   == ndim
-	sig = rho.^abs(linspace(1,ndim,ndim) .- linspace(1,ndim,ndim)')
+	sig = rho.^abs.(linspace(1,ndim,ndim) .- linspace(1,ndim,ndim)')
 	@test c.sigma == sig
 
 	ndim = 2
@@ -21,7 +21,7 @@ print("Testing with tolerance level = $(tolTest)")
 	c = NormalCopula(ndim,rho)
 	@test c.rho == rho
 	@test c.d   == ndim
-	sig = rho.^abs(linspace(1,ndim,ndim) .- linspace(1,ndim,ndim)')
+	sig = rho.^abs.(linspace(1,ndim,ndim) .- linspace(1,ndim,ndim)')
 	@test c.sigma == sig
 
 end
@@ -45,14 +45,14 @@ end
 # cop = normalCopula(0.5)
 # Rdcopula = dCopula(as.matrix(vals), cop)
 
-	Rdcopula = [1.154701*1 for i=1:4]	
+	Rdcopula = [1.154701*1 for i=1:4]
 
 	@test dnormCopula(u2,cop)[:] ≈ Rdcopula[:] atol = tolTest
 
 	# rho = .9
 	rho  = 0.9
 	cop = NormalCopula(ndim,rho)
-	Rdcopula = [2.294157*1 for i=1:4]	
+	Rdcopula = [2.294157*1 for i=1:4]
 	@test dnormCopula(u2,cop)[:]  ≈ Rdcopula[:] atol = tolTest
 
 	# n = 3. 3 points
@@ -61,8 +61,7 @@ end
 	cop = NormalCopula(ndim,rho)
 	u = linspace(1/n, 1-1/n, n)
 	u2 =[repmat(u,n,1) repmat(u,1,n)'[:] ]
-	Rdcopula = [1.2283 1.1195 0.9591 1.1195 1.1547 1.1195 0.9591 1.1195 1.2283]	
+	Rdcopula = [1.2283 1.1195 0.9591 1.1195 1.1547 1.1195 0.9591 1.1195 1.2283]
 	@test dnormCopula(u2,cop)[:]  ≈ Rdcopula[:] atol = tolTest
 
 end
-
